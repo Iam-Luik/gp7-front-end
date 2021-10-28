c78b92795d4cd9fc455fd7b8a05f8d2e19606ec3
Rennan-Aquino <rennan.silva@bandtec.com.br>
2021-10-28 19:55:17 -0300

    ajustes

--- site/src/pages/direcionamento/style.js
+++ site/src/pages/direcionamento/style.js
@@ -13,6 +13,7 @@ export const RowBlock = styled.div`
     box-shadow: 0px 0px 51px -20px rgba(0,0,0,0.75);
     border-radius: 40px;
     width: 300px;
+    
    
     a{
       text-decoration: none;

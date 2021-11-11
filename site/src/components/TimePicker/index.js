f6ea6d5648a89dd12c6ecee70809bc0dc980815f
GabrielaFoschini <gabriela.foschini@gmail.com>
2021-11-10 22:06:29 -0300

    fix: input datapicker

--- site/src/components/TimePicker/index.js
+++ site/src/components/TimePicker/index.js
@@ -1,9 +1,11 @@
 import "./style.css";
 
 export default function TimePicker() {
+  // var form = document.querySelector("#texto");
+  // console.log(form.value);
   return (
     <>
-    <input type="time" />
+      <input type="time" id="texto" />
     </>
   );
 }

fede1ba4f650434d701f84cde0ef6dc7b08134f4
iam-luik <castro.luik@hotmail.com>
2021-11-13 18:48:48 -0300

    feat: tela de recuperação de senha

--- site/src/pages/Codigo/index.js
+++ site/src/pages/Codigo/index.js
@@ -1,62 +1,52 @@
-import React, { useState } from 'react';
-import Navbar from "../../components/Navbar";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
 import Footer from "../../components/Footer";
-import GeneralInput from "../../components/GeneralInput";
-import InputFile from "../../components/InputFile";
-import GeneralInputPssw from "../../components/GeneralInputPssw";
 import GeneralButton from "../../components/GeneralButton";
-import ButtonFile from "../../components/ButtonFile";
-import clips from "../../assets/paper-clip.png";
-import txtIcone from "../../assets/txtIcone.png";
-import Api from "../../services/api"
-
-import { RowBlocks, RowBlock, InputContainer, TxtContainer } from "./style";
-import { Link } from "react-router-dom";
-import { useHistory } from 'react-router';
+import GeneralInput from "../../components/GeneralInput";
+import Navbar from "../../components/Navbar";
+import { InputContainer, RowBlock } from "./style";
 
 /** Padrão de formulários a ser seguidos no projeto */
 const Codigo = () => {
-    /** Define os get e set dos valores */
-const history = useHistory();
-const [codigoDigitado, setCodigoDigitado] = useState("");
+  /** Define os get e set dos valores */
+  const history = useHistory();
+  const [codigoDigitado, setCodigoDigitado] = useState("");
 
- function validar(e) {
+  function validar(e) {
     e.preventDefault();
-   if(codigoDigitado === localStorage.getItem("cod")){
-        localStorage.setItem("cod", "sdhfugiuaurhguwieou");
-                history.push('/senha');
-   }else{
-       alert("código incorreto!")
-   }
-    localStorage.setItem("cod", "xphauigfuashoisdhgyuahsid");
-  
+    if (codigoDigitado === sessionStorage.getItem("cod")) {
+      sessionStorage.setItem("cod", Math.random() * (1000000 - 10000) + 10000);
+      history.push("/senha");
+    } else {
+      alert("código incorreto!");
+    }
+    sessionStorage.setItem("cod", Math.random() * (1000000 - 10000) + 10000);
   }
 
-    return (
-        <>
-            <Navbar />
-            <RowBlock>
-                <h1>ATUALIZAÇÃO DE SENHA</h1>
-                <p>Digite o código enviado para seu email para validarmos sua nova senha</p>
+  return (
+    <>
+      <Navbar />
+      <RowBlock>
+        <h1>ATUALIZAÇÃO DE SENHA</h1>
+        <p>
+          Digite o código enviado para seu email para validarmos sua nova senha
+        </p>
 
+        <form onSubmit={validar}>
+          <InputContainer>
+            <GeneralInput
+              label="Digite o código *"
+              onChange={(e) => setCodigoDigitado(e.target.value)}
+            />
+          </InputContainer>
 
-                <form onSubmit={validar}>
-                <InputContainer>
-                    <GeneralInput label="Digite o código *" onChange={e => setCodigoDigitado(e.target.value)} />
-                </InputContainer>
+          <GeneralButton type="submit" button="Continuar" />
+        </form>
+      </RowBlock>
 
-             
-                    <GeneralButton type="submit" button="Continuar" />
-                
-                </form>
-
-            </RowBlock>
-
-
-            <Footer />
-        </>
-    );
-}
-
+      <Footer />
+    </>
+  );
+};
 
 export default Codigo;
--- site/src/pages/Senha/index.js
+++ site/src/pages/Senha/index.js
@@ -1,68 +1,65 @@
-import React, { useState } from 'react';
-import Navbar from "../../components/Navbar";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
 import Footer from "../../components/Footer";
-import GeneralInput from "../../components/GeneralInput";
-import InputFile from "../../components/InputFile";
-import GeneralInputPssw from "../../components/GeneralInputPssw";
 import GeneralButton from "../../components/GeneralButton";
-import ButtonFile from "../../components/ButtonFile";
-import clips from "../../assets/paper-clip.png";
-import txtIcone from "../../assets/txtIcone.png";
-import Api from "../../services/api"
-import { useHistory } from 'react-router';
-
-
-import { RowBlocks, RowBlock, InputContainer, TxtContainer } from "./style";
-import { Link } from "react-router-dom";
+import GeneralInput from "../../components/GeneralInput";
+import Navbar from "../../components/Navbar";
+import Api from "../../services/api";
+import { InputContainer, RowBlock } from "./style";
 
 /** Padrão de formulários a ser seguidos no projeto */
 const Senha = () => {
+  const history = useHistory();
+  const [senha, setSenha] = useState("");
 
-const history = useHistory();
-const [senha, setSenha] = useState("");
-
-function alterarSenha(e) {
+  function alterarSenha(e) {
     e.preventDefault();
-    Api.put("http://localhost:8080/usuario/alterar-senha/" + localStorage.getItem("email") + "/"+ senha, {}).then((resposta) => {
-      if (resposta.status === 200) {
-        localStorage.setItem("cod", resposta.data);
-                history.push('/login');
-      }else{
-        alert("Senha não foi alterada")
-      }
-    }).catch((erro) => {
-      console.log(erro)
-    })
-  
+    Api.put(
+      "http://localhost:8080/usuario/alterar-senha/" +
+        sessionStorage.getItem("email") +
+        "/" +
+        senha,
+      {}
+    )
+      .then((resposta) => {
+        if (resposta.status === 200) {
+          sessionStorage.setItem("cod", resposta.data);
+          history.push("/login");
+        } else {
+          alert("Senha não foi alterada");
+        }
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
   }
 
-    /** Define os get e set dos valores */
-    return (
-        <>
-            <Navbar />
-            <RowBlock>
-                <h1>ATUALIZAÇÃO DE SENHA</h1>
-                <p>Digite sua nova senha para finalizar</p>
+  /** Define os get e set dos valores */
+  return (
+    <>
+      <Navbar />
+      <RowBlock>
+        <h1>ATUALIZAÇÃO DE SENHA</h1>
+        <p>Digite sua nova senha para finalizar</p>
 
-               
         <form onSubmit={alterarSenha}>
-                    <InputContainer>
-                    <GeneralInput label="Digite sua nova senha *" onChange={e => setSenha(e.target.value)} />
-                    </InputContainer>
-                    <InputContainer>
-                    <GeneralInput label="Confirme sua senha *" onChange={""} />
-                    </InputContainer>
-                    
-                <GeneralButton type="submit" button="Continuar" />
-                </form>
-               
-            </RowBlock>
+          <InputContainer>
+            <GeneralInput
+              label="Digite sua nova senha *"
+              onChange={(e) => setSenha(e.target.value)}
+            />
+          </InputContainer>
+          <InputContainer>
+            <GeneralInput label="Confirme sua senha *" onChange={""} />
+          </InputContainer>
 
+          <GeneralButton type="submit" button="Continuar" />
+        </form>
+      </RowBlock>
 
-            <Footer />
-        </>
-    );
-}
-
+      <Footer />
+    </>
+  );
+};
 
 export default Senha;

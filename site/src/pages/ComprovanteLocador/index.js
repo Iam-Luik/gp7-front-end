42f5c14296732140773ff398cbda9faef3ded129
GabrielaFoschini <gabriela.foschini@gmail.com>
2021-11-27 19:06:11 -0300

    fix: dinamic pages

--- site/src/pages/ComprovanteLocador/index.js
+++ site/src/pages/ComprovanteLocador/index.js
@@ -4,6 +4,9 @@ import Footer from "../../components/Footer";
 import Countdown from "../../components/Countdown";
 import GeneralButton from "../../components/GeneralButton";
 import NavbarPadrao from "../../components/NavbarPadrao";
+import Api from "../../services/api";
+import React, { useState, useEffect } from "react";
+
 import {
   ColBlocks,
   RowBlockLeft,
@@ -12,6 +15,19 @@ import {
 } from "./style";
 
 const ComprovanteLocador = () => {
+  const [dados, setDados] = useState({bicicleta: {usuario: {endereco: {}}}});
+
+  useEffect(() => {
+    Api.get("http://localhost:8080/locacao/consultar-locacao/" + sessionStorage.getItem("idLocacao"))
+    .then((response) => {
+      // console.log(response.data)
+      setDados(response.data)
+    })
+    .catch((err) => {
+      console.error("ops! ocorreu um erro" + err);
+    });
+  }, []);
+
   return (
     <>
       <NavbarPadrao />
@@ -20,33 +36,30 @@ const ComprovanteLocador = () => {
       </TitleComprovante>
       <ColBlocks>
         <RowBlockLeft>
-          <h1>Bicicleta Caloi Two Niner</h1>
-          <h2>Aro 29</h2>
-          <h1>
-            <i>R$ 00,00</i>
-          </h1>
+          <h1>{dados.bicicleta.marca}</h1>
+          <h2>{dados.bicicleta.tamanhoAro}</h2>
 
           <h4>
-            <b>LOCADOR:</b> <span> Vitória Jeniffer</span>
+            <b>LOCADOR:</b> <span>{dados.bicicleta.usuario.nome} {dados.bicicleta.usuario.sobrenome}</span>
           </h4>
           <h4>
             <b>LOCATÁRIO:</b> <span>Julio Vitor Silveira</span>
           </h4>
           <h4>
-            <b>BICICLETA:</b> <span>Caloi Two Niner</span>
+            <b>BICICLETA:</b> <span>{dados.bicicleta.marca} {dados.bicicleta.modelo}</span>
           </h4>
           <h4>
-            <b>CEP:</b> <span>03447-170</span>
+            <b>CEP:</b> <span>{dados.bicicleta.usuario.endereco.cep}</span>
           </h4>
           <h4>
-            <b>ENDEREÇO:</b> <span>Rua Camilo Mazza</span>
+            <b>ENDEREÇO:</b> <span>{dados.bicicleta.usuario.endereco.rua} - {dados.bicicleta.usuario.endereco.numero}</span>
           </h4>
           <h4>
-            <b>BAIRRO:</b> <span>Vila Carrão</span>
+            <b>BAIRRO:</b> <span>{dados.bicicleta.usuario.endereco.bairro}</span>
           </h4>
         </RowBlockLeft>
         <RowBlockRight>
-          <img src={Bibi} alt="" />
+          <img src={"http://localhost:8080/bicicleta/bicicleta-imagem/" + dados.bicicleta.id} alt="" width={"400px"}/>
           <Countdown />
           <Link to="/card">
             <GeneralButton button="CONFIRMAR DEVOLUÇÃO" />
